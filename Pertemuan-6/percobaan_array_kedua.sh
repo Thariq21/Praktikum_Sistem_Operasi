a0bf85a065156d7cccd8892cde2a0f94cbb2c8f0
guest <guest@os.tifubakrie.ac.id>
2024-11-10 17:54:41 +0700

    Penambahan percobaan_array_kedua

--- Pertemuan-6/percobaan_array_kedua.sh
+++ Pertemuan-6/percobaan_array_kedua.sh
@@ -1,1 +1,38 @@
 FRUITS=""
+FRUITS="${FRUITS} PEPAYA"
+FRUITS="${FRUITS} MANGGA"
+FRUITS="${FRUITS} PISANG"
+FRUITS="${FRUITS} JAMBU"
+FRUITS="${FRUITS} JERUK"
+FRUITS="${FRUITS} KIWI"
+FRUITS="${FRUITS} APEL"
+FRUITS="${FRUITS} STRAWBERRY"
+
+echo 'Inilah daftar buah-buahan...'
+for BUAH in ${FRUITS}; do
+	echo ${BUAH};
+done
+
+echo 'Sekarang daftar semua buah adalah : '
+echo ${FRUITS}
+
+echo 'Nama buah kedua adalah : '
+echo ${FRUITS} | cut -d' ' -f2
+
+echo 'Nama buah keempat adalah : '
+echo ${FRUITS} | cut -d' ' -f4
+
+echo 'Tambahkan 1 nama buah...'
+FRUITS="${FRUITS} DURIAN"
+
+echo 'Nama buah kesembilan adalah : '
+echo ${FRUITS} | cut -d' ' -f9
+
+echo 'Nama buah kesepuluh adalah : '
+echo ${FRUITS} | cut -d' ' -f10
+
+echo 'Tambahkan 1 lagi nama buah...'
+FRUITS="${FRUITS} DUKU"
+
+echo 'Nama buah kesepuluh adalah : '
+echo ${FRUITS} | cut -d' ' -f10
